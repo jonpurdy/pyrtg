61aab49219672b85750ec18f53106ddcd78dd888
Jon Purdy <jon@knowroaming.com>
2017-10-12 15:08:00 -0400

    Removed the reading of README and LICENSE in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@
 
 from setuptools import setup, find_packages
 
+# with open('README.rst') as f:
+#     readme = f.read()
 
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
+# with open('LICENSE') as f:
+#     license = f.read()
 
 setup(
     name='pyrtg',
@@ -17,7 +16,7 @@ setup(
     description='Classes for easily generating PRTG XML output.',
     long_description=readme,
     author='Jon Purdy',
-    author_email='jon@jonpurdy.com',
+    author_email='jon+pyrtg@jonpurdy.com',
     url='https://github.com/jonpurdy/pyrtg',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
